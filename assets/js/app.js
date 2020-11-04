4ebc5ebd0db96f9de25d74e9bc2df37308285749
Yoangel Eizaga <38538698+EizagaYC@users.noreply.github.com>
2020-11-04 15:03:49 -0400

    Mejorar el diseño de los cursos realizados
    Agregue una vista mas cómoda para el usuario haciendo un slider el cual da mes experiencia el verlo

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    // Guardamos el modo en localstorage.
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
+
+if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+
+$('.owl-carousel').owlCarousel({
+    center: true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    loop:true,
+
+    responsive:{
+      0:{
+            items:2,
+        dots:false
+        },
+        576:{
+            items:4,
+            dots:true
+        },
+        992:{
+          items:4,
+          dots:false
+        }
+    }
+
+});
